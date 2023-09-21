2f01e986ba5046776ee3ec3bb1b48acbf7290b9b
Pat101404 <pattoo10104@gmail.com>
2023-09-21 12:08:20 +0200

    some php

---
+++ web/index.php
@@ -0,0 +1,8 @@
+<html>
+<head>
+  <link rel="stylesheet" href="">
+</head>
+<body>
+
+</body>
+</html>
---
+++ web/login/login.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+  <link rel="stylesheet" href="login.css">
+</head>
+<body>
+
+<div class="login-box">
+    <form>
+        <div class="user-box">
+            <input type="text" name="" required="">
+            <label>Username</label>
+        </div>
+        <div class="user-box">
+            <input type="password" name="" required="">
+            <label>Password</label>
+        </div><center>
+        <a href="#">
+            SEND
+            <span></span>
+        </a></center>
+    </form>
+</div>
+
+</body>
+</html>
