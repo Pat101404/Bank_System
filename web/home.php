4a99c5bf9bdadae900ab408e0c82ac449d1ba56c
Pat101404 <pattoo10104@gmail.com>
2023-09-26 10:20:04 +0200

    pages, todo list
    added a few pages and a todo list

--- web/home.php
+++ web/home.php
@@ -1,174 +1,54 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-<title>W3.CSS Template</title>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FlowBank - Home</title>
+    <link rel="stylesheet" href="css/main.css">
 </head>
 <body>
+    <header>
+        <h1>Welcome to Bank Name</h1>
+        <nav>
+            <ul>
+                <li><a href="#home.php">Home</a></li>
+                <li><a href="#accounts.php">Accounts</a></li>
+                <li><a href="#loans.php">Loans</a></li>
+                <li><a href="#services.php">Services</a></li>
+                <li><a href="#contacts.php">Contact Us</a></li>
+            </ul>
+        </nav>
+    </header>
 
-<!-- Page Content -->
-<div id="main">
+    <section id="main-content">
+        <h2>Featured Services</h2>
+        <div class="service">
+            <h3>Online Banking</h3>
+            <p>Manage your accounts online 24/7.</p>
+            <a href="#">Learn More</a>
+        </div>
 
-  <!-- Header/Home -->
-  <header>
-    <h1>I'm John Doe.</h1>
-    <p>Photographer and Web Designer.</p>
-    <img src="/w3images/man_smoke.jpg" alt="boy" width="992" height="1108">
-  </header>
+        <div class="service">
+            <h3>Mortgage Loans</h3>
+            <p>Get your dream home with our flexible mortgage plans.</p>
+            <a href="#">Apply Now</a>
+        </div>
 
-  <!-- About Section -->
-  <div>
-    <h2>My Name</h2>
-    <hr style="width:200px;">
-    <p>Some text about me. Some text about me. I am lorem ipsum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-      ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur
-      adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-    </p>
-    <h3>My Skills</h3>
-    <p>Photography</p>
-    <div style="height:28px; width:95%"></div>
-    <p>Web Design</p>
-    <div style="height:28px; width:85%"></div>
-    <p>Photoshop</p>
-    <div style="height:28px; width:80%"></div><br>
-    
-    <div>
-      <div>
-        <span>11+</span><br>
-        Partners
-      </div>
-      <div>
-        <span>55+</span><br>
-        Projects Done
-      </div>
-      <div>
-        <span>89+</span><br>
-        Happy Clients
-      </div>
-      <div>
-        <span>150+</span><br>
-        Meetings
-      </div>
-    </div>
+    </section>
 
-    <button>
-      <i></i> Download Resume
-    </button>
-    
-    <!-- Grid for pricing tables -->
-    <h3>My Price</h3>
-    <div>
-      <div>
-        <ul>
-          <li>Basic</li>
-          <li>Web Design</li>
-          <li>Photography</li>
-          <li>5GB Storage</li>
-          <li>Mail Support</li>
-          <li>
-            <h2>$ 10</h2>
-            <span>per month</span>
-          </li>
-          <li>
-            <button>Sign Up</button>
-          </li>
-        </ul>
-      </div>
+    <section id="contact">
+        <h2>Contact Us</h2>
+        <p>If you have any questions or need assistance, please don't hesitate to contact us.</p>
+        <address>
+            <p>123 Bank Street</p>
+            <p>City, State ZIP Code</p>
+            <p>Email: <a href="mailto:">info@bankname.com</a></p>
+            <p>Phone: <a href="tel:+"></a></p>
+        </address>
+    </section>
 
-      <div>
-        <ul>
-          <li>Pro</li>
-          <li>Web Design</li>
-          <li>Photography</li>
-          <li>50GB Storage</li>
-          <li>Endless Support</li>
-          <li>
-            <h2>$ 25</h2>
-            <span>per month</span>
-          </li>
-          <li>
-            <button>Sign Up</button>
-          </li>
-        </ul>
-      </div>
-    <!-- End Grid/Pricing tables -->
-    </div>
-    
-    <!-- Testimonials -->
-    <h3>My Reputation</h3>  
-    <img src="/w3images/bandmember.jpg" alt="Avatar" style="width:80px;">
-    <p><span>Chris Fox.</span> CEO at Mighty Schools.</p>
-    <p>John Doe saved us from a web disaster.</p><br>
-    
-    <img src="/w3images/avatar_g2.jpg" alt="Avatar" style="width:80px;">
-    <p><span>Rebecca Flex.</span> CEO at Company.</p>
-    <p>No one is better than John Doe.</p>
-  <!-- End About Section -->
-  </div>
-  
-  <!-- Portfolio Section -->
-  <div>
-    <h2>My Photos</h2>
-    <hr style="width:200px;">
-    <!-- Grid for photos -->
-    <div>
-      <div>
-        <img src="/w3images/wedding.jpg" style="width:100%;">
-        <img src="/w3images/rocks.jpg" style="width:100%;">
-        <img src="/w3images/sailboat.jpg" style="width:100%;">
-      </div>
-
-      <div>
-        <img src="/w3images/underwater.jpg" style="width:100%;">
-        <img src="/w3images/chef.jpg" style="width:100%;">
-        <img src="/w3images/wedding.jpg" style="width:100%;">
-        <img src="/w3images/p6.jpg" style="width:100%;">
-      </div>
-    <!-- End photo grid -->
-    </div>
-  <!-- End Portfolio Section -->
-  </div>
-
-  <!-- Contact Section -->
-  <div>
-    <h2>Contact Me</h2>
-    <hr style="width:200px;">
-    <div>
-      <p><i style="font-size: 24px;"></i> Chicago, US</p>
-      <p><i style="font-size: 24px;"></i> Phone: +00 151515</p>
-      <p><i style="font-size: 24px;"> </i> Email: mail@mail.com</p>
-    </div><br>
-    <p>Let's get in touch. Send me a message:</p>
-
-    <form action="/action_page.php" target="_blank">
-      <p><input type="text" placeholder="Name" required name="Name"></p>
-      <p><input type="text" placeholder="Email" required name="Email"></p>
-      <p><input type="text" placeholder="Subject" required name="Subject"></p>
-      <p><input type="text" placeholder="Message" required name="Message"></p>
-      <p>
-        <button type="submit">
-          <i></i> SEND MESSAGE
-        </button>
-      </p>
-    </form>
-  <!-- End Contact Section -->
-  </div>
-  
-    <!-- Footer -->
-  <footer>
-    <i class="facebook-official"></i>
-    <i class="instagram"></i>
-    <i class="snapchat"></i>
-    <i class="pinterest-p"></i>
-    <i class="twitter"></i>
-    <i class="linkedin"></i>
-    <p>Powered by <a href="https://www.w3schools.com/w3css/default.asp" target="_blank">w3.css</a></p>
-  <!-- End footer -->
-  </footer>
-
-<!-- END PAGE CONTENT -->
-</div>
-
+    <footer>
+        <p>&copy; 2023 Bank Name. All rights reserved.</p>
+    </footer>
 </body>
 </html>
