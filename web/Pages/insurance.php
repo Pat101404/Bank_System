e0fa3cc3e0d738f490ce1a68431a0cd8891ab25b
Pat101404 <pattoo10104@gmail.com>
2023-10-03 11:27:31 +0200

    Insurance page basic

--- web/Pages/insurance.php
+++ web/Pages/insurance.php
@@ -14,7 +14,45 @@
     ?>
 
     <div>
-
+        <div>
+            <h3>Insurance</h3>
+            <div class="travel-insurance">
+                <h4>Travel insurance</h4>
+            </div>
+            <div class="life-insurance">
+                <h4>Life insurance</h4>
+            </div>
+            <div class="medical-insurance">
+                <h4>Medical insurance</h4>
+            </div>
+            <div class="property-insurance">
+                <h4>Property insurance</h4>
+            </div>
+            <div class="law-insurance">
+                <h4>Law insurance</h4>
+            </div>
+            <div class="business-insurance">
+                <h4>Business insurance</h4>
+            </div>
+            <div class="car-insurance">
+                <h4>Car insurance</h4>
+            </div>
+            <div class="accident-insurance">
+                <h4>Accident insurance</h4>
+            </div>
+        </div>
+        <div>
+            <ul class="insurance-list">
+                <li><a>Travel insurance</a></li>
+                <li><a>Life insurance</a></li>
+                <li><a>Medical insurance</a></li>
+                <li><a>Property insurance</a></li>
+                <li><a>Law insurance</a></li>
+                <li><a>Business insurance</a></li>
+                <li><a>Car insurance</a></li>
+                <li><a>Accident insurance</a></li>
+            </ul>
+        </div>
     </div>
 
     <?php 
