a6e5d1a1e66b1da585e1ce18f57c7ddcf2b139ed
Pat101404 <pattoo10104@gmail.com>
2023-10-03 10:55:25 +0200

    individual head

--- web/Pages/insurance.php
+++ web/Pages/insurance.php
@@ -1,6 +1,11 @@
-<?php 
-    require "../templates/head-no1.php";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/nav-buttons.css">
+    <link rel="stylesheet" href="../css/main.css">
+    <link rel="icon" href="../imgs/svg/logo-no-background.svg">
     <title>FlowBank - Insurance</title>
 </head>
 <body>
