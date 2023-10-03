275452566481f42197bf9153a8af7b6038e85722
Pat101404 <pattoo10104@gmail.com>
2023-10-03 13:35:28 +0200

    cards page update

--- web/Pages/cards.php
+++ web/Pages/cards.php
@@ -18,12 +18,12 @@
         <div>   
 
         </div>
-        <div class="credit-card-window">
-            <h4>Credit Card</h4>
-            <div class="credit-card-image">
-                <img src="" alt="credit-card-image">
+        <h4>Credit Card</h4>
+        <div class="card-window">
+            <div class="card-image">
+                <img src="../imgs/png/credit-card.png" alt="credit-card-image">
             </div>
-            <div class="credit-card-info">
+            <div class="card-info">
                 <ul>
                     <li>Tempor quis est minim minim voluptate officia proident.</li>
                     <li>Anim id sit exercitation eiusmod sint aliqua.</li>
@@ -31,18 +31,32 @@
                 </ul>
             </div>  
         </div>
-        <div class="debit-card-window">
-            <h4>Credit Card</h4>
-            <div class="debit-card-image">
-                <img src="" alt="debit-card-image">    
+        <h4>Debit Card</h4>
+        <div class="card-window">
+            <div class="card-image">
+                <img src="../imgs/png/credit-card.png" alt="debit-card-image">    
             </div>
-            <div class="debit-card-info">
+            <div class="card-info">
                 <ul>
                     <li>Laborum eiusmod dolor nostrud sit consequat consectetur quis pariatur.</li>
                     <li>Ex dolor dolore anim ex eu nisi reprehenderit irure sit non.</li>
                     <li>Aliquip cupidatat elit nostrud officia cupidatat ex occaecat ex mollit consequat deserunt irure.</li>
                 </ul>
             </div>  
+        </div>
+        <div>
+            <div>
+                <ul>
+                    <li><a></a></li>
+                    <li><a></a></li>
+                </ul>
+            </div>
+            <div>
+                <ul>
+                    <li><a></a></li>
+                    <li><a></a></li>
+                </ul>
+            </div>
         </div>
     </div>
 
