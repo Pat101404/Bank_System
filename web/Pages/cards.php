ec71e47c2143a9ca1536b1a316b919205386cc11
Pat101404 <pattoo10104@gmail.com>
2023-10-03 10:48:26 +0200

    card page basics

--- web/Pages/cards.php
+++ web/Pages/cards.php
@@ -9,7 +9,36 @@
     ?>
 
     <div>
+        <h3>Cards</h3>
+        <div>   
 
+        </div>
+        <div class="credit-card-window">
+            <h4>Credit Card</h4>
+            <div class="credit-card-image">
+                <img src="" alt="credit-card-image">
+            </div>
+            <div class="credit-card-info">
+                <ul>
+                    <li>info</li>
+                    <li>info info</li>
+                    <li>info info info</li>
+                </ul>
+            </div>  
+        </div>
+        <div class="debit-card-window">
+            <h4>Credit Card</h4>
+            <div class="debit-card-image">
+                <img src="" alt="debit-card-image">    
+            </div>
+            <div class="debit-card-info">
+                <ul>
+                    <li>info</li>
+                    <li>info info</li>
+                    <li>info info info</li>
+                </ul>
+            </div>  
+        </div>
     </div>
 
     <?php 
