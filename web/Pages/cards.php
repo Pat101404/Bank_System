90866fa7d3a2bbd4cf622e666adf92eaea77c09d
Pat101404 <pattoo10104@gmail.com>
2023-10-04 12:41:24 +0200

    small overhaul
    css for cards, comments, global head

--- web/Pages/cards.php
+++ web/Pages/cards.php
@@ -1,52 +1,125 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/nav-buttons.css">
-    <link rel="stylesheet" href="../css/main.css">
-    <link rel="icon" href="../imgs/svg/logo-no-background.svg">
+    <?php 
+        require "../templates/head.html";
+    ?>
     <title>FlowBank - Cards</title>
 </head>
 <body>
+<!-- navbar -->
     <?php 
         require "../templates/nav.php";
     ?>
 
     <div class="core">
-        <h3>Cards</h3>
-        <div>   
-
+<!-- horizontal card panels -->
+        <div>
+<!-- wide credit card -->
+            <a class="card-link" href="">
+                <div class="card-window-h">
+                    <div class="card-image">
+                        <img src="../imgs/png/credit-card.png" alt="credit-card-image">
+                    </div>
+                    <div class="card-info">
+                        <ul>
+                            <h4>Credit Card</h4>
+                            <li>Tempor quis est minim minim voluptate officia proident.</li>
+                            <li>Anim id sit exercitation eiusmod sint aliqua.</li>
+                            <li>Est aute tempor aliqua anim pariatur laborum voluptate do id irure esse reprehenderit sint.</li>
+                        </ul>
+                    </div>  
+                </div>
+            </a>
+<!-- wide debit card -->
+            <a class="card-link" href="">
+                <div class="card-window-h">
+                    <div class="card-image">
+                        <img src="../imgs/png/credit-card.png" alt="debit-card-image">    
+                    </div>
+                    <div class="card-info">
+                        <ul>
+                            <h4>Debit Card</h4>
+                            <li>Laborum eiusmod dolor nostrud sit consequat consectetur quis pariatur.</li>
+                            <li>Ex dolor dolore anim ex eu nisi reprehenderit irure sit non.</li>
+                            <li>Aliquip cupidatat elit nostrud officia cupidatat ex occaecat ex mollit consequat deserunt irure.</li>
+                        </ul>
+                    </div>  
+                </div>
+            </a>
         </div>
-        <h4>Credit Card</h4>
-        <div class="card-window">
-            <div class="card-image">
-                <img src="../imgs/png/credit-card.png" alt="credit-card-image">
+<!-- vertical card panels -->
+        <div>
+<!-- tall credit cards row1 -->
+            <div class="card-row">
+                <a class="card-link" href="">
+                    <div class="card-window-v">
+                        <div class="card-image">
+                            <img src="../imgs/png/credit-card.png" alt="credit-card-image">
+                        </div>
+                        <div class="card-info">
+                            <ul>
+                                <h4>Credit Card</h4>
+                                <li>Tempor quis est minim minim voluptate officia proident.</li>
+                                <li>Anim id sit exercitation eiusmod sint aliqua.</li>
+                                <li>Est aute tempor aliqua anim pariatur laborum voluptate do id irure esse reprehenderit sint.</li>
+                            </ul>
+                        </div>  
+                    </div>
+                </a>
+                <a class="card-link" href="">
+                    <div class="card-window-v">
+                        <div class="card-image">
+                            <img src="../imgs/png/credit-card.png" alt="debit-card-image">    
+                        </div>
+                        <div class="card-info">
+                            <ul>
+                                <h4>Debit Card</h4>
+                                <li>Laborum eiusmod dolor nostrud sit consequat consectetur quis pariatur.</li>
+                                <li>Ex dolor dolore anim ex eu nisi reprehenderit irure sit non.</li>
+                                <li>Aliquip cupidatat elit nostrud officia cupidatat ex occaecat ex mollit consequat deserunt irure.</li>
+                            </ul>
+                        </div>  
+                    </div>
+                </a>
             </div>
-            <div class="card-info">
-                <ul>
-                    <li>Tempor quis est minim minim voluptate officia proident.</li>
-                    <li>Anim id sit exercitation eiusmod sint aliqua.</li>
-                    <li>Est aute tempor aliqua anim pariatur laborum voluptate do id irure esse reprehenderit sint.</li>
-                </ul>
-            </div>  
-        </div>
-        <h4>Debit Card</h4>
-        <div class="card-window">
-            <div class="card-image">
-                <img src="../imgs/png/credit-card.png" alt="debit-card-image">    
+<!-- tall credit cards row2 -->
+            <div class="card-row">
+                <a class="card-link" href="">
+                    <div class="card-window-v">
+                        <div class="card-image">
+                            <img src="../imgs/png/credit-card.png" alt="credit-card-image">
+                        </div>
+                        <div class="card-info">
+                            <ul>
+                                <h4>Credit Card</h4>
+                                <li>Tempor quis est minim minim voluptate officia proident.</li>
+                                <li>Anim id sit exercitation eiusmod sint aliqua.</li>
+                                <li>Est aute tempor aliqua anim pariatur laborum voluptate do id irure esse reprehenderit sint.</li>
+                            </ul>
+                        </div>  
+                    </div>
+                </a>
+                <a class="card-link" href="">
+                    <div class="card-window-v">
+                        <div class="card-image">
+                            <img src="../imgs/png/credit-card.png" alt="debit-card-image">    
+                        </div>
+                        <div class="card-info">
+                            <ul>
+                                <h4>Debit Card</h4>
+                                <li>Laborum eiusmod dolor nostrud sit consequat consectetur quis pariatur.</li>
+                                <li>Ex dolor dolore anim ex eu nisi reprehenderit irure sit non.</li>
+                                <li>Aliquip cupidatat elit nostrud officia cupidatat ex occaecat ex mollit consequat deserunt irure.</li>
+                            </ul>
+                        </div>  
+                    </div>
+                </a>
             </div>
-            <div class="card-info">
-                <ul>
-                    <li>Laborum eiusmod dolor nostrud sit consequat consectetur quis pariatur.</li>
-                    <li>Ex dolor dolore anim ex eu nisi reprehenderit irure sit non.</li>
-                    <li>Aliquip cupidatat elit nostrud officia cupidatat ex occaecat ex mollit consequat deserunt irure.</li>
-                </ul>
-            </div>  
         </div>
+<!-- card list -->
         <div>
             <div>
-
                 <ul>
                     <li><a>CLassic Debit Card</a></li>
                     <li><a>Classic Credit Card</a></li>
@@ -60,7 +133,7 @@
             </div>
         </div>
     </div>
-
+<!-- footer -->
     <?php 
         require "../templates/footer.php";
     ?>
--- web/Pages/home.php
+++ web/Pages/home.php
@@ -1,11 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/nav-buttons.css">
-    <link rel="stylesheet" href="../css/main.css">
-    <link rel="icon" href="../imgs/svg/logo-no-background.svg">
+    <?php 
+        require "../templates/head.html";
+    ?>
     <title>FlowBank - Home</title>
 </head>
 <body>
