9c127c05dc5d5943267c8c71075bda56baf5921e
Pat101404 <pattoo10104@gmail.com>
2023-11-02 23:13:47 +0100

    logo folder

--- web/v2/templates/sidebar.php
+++ web/v2/templates/sidebar.php
@@ -5,7 +5,7 @@
     </a>
     <ul class="side-menu">
         <li class="active"><a onclick="toggleElement('overview')" ><i class='bx bxs-dashboard'></i>Overview</a></li>
-        <li><a onclick="toggleElement('transfers')" ><i class='bx bx-transfer-alt'></i>Transfers</a></li>
+        <li><a onclick="toggleElement('transactions')" ><i class='bx bx-transfer-alt'></i>Transfers</a></li>
         <li><a onclick="toggleElement('cards')"><i class='bx bx-credit-card'></i>Cards</a></li>
         <li><a onclick="toggleElement('loans')"><i class='bx bx-dollar'></i>Loans</a></li>
         <li><a onclick="toggleElement('account')"><i class='bx bx-user'></i>Account</a></li>
--- web/v2/templates/transactions.php
+++ web/v2/templates/transactions.php
@@ -15,26 +15,59 @@
             </h3>
         </div>
         <table>
-            <thead>
+        <thead>
                 <tr>
-                    <th>name</th>
-                    <th>status</th>
-                    <th>status</th>
-                    <th>status</th>
-                    <th>status</th>
-                    <th>status</th>
+                    <th>Card</th>
+                    <th>Recipient</th>
+                    <th>Transaction Time</th>
+                    <th>Type</th>
+                    <th>Amount</th>
+                    <th>Status</th>
                 </tr>
             </thead>
             <tbody>
                 <tr>
-                    <td>name</td>
+                    <td>Card #1</td>
+                    <td>John Doe</td>
+                    <td>15:42 | 14/08/2023</td>
+                    <td>transaction</td>
+                    <td>100,000</td>
                     <td><span class="status completed">Completed</span></td>
+                </tr>
+                <tr>
+                    <td>Card #3</td>
+                    <td>John Doe</td>
+                    <td>15:42 | 14/08/2023</td>
+                    <td>transaction</td>
+                    <td>-20,000</td>
                     <td><span class="status pending">Pending</span></td>
+                </tr>
+                <tr>
+                    <td>Card #2</td>
+                    <td>John Doe</td>
+                    <td>15:42 | 14/08/2023</td>
+                    <td>transaction</td>
+                    <td>10,000</td>
                     <td><span class="status process">Processing</span></td>
+                </tr>
+                <tr>
+                    <td>Card #1</td>
+                    <td>John Doe</td>
+                    <td>15:42 | 14/08/2023</td>
+                    <td>transaction</td>
+                    <td>50,000</td>
                     <td><span class="status failed">Failled</span></td>
+                </tr>
+                <tr>
+                    <td>Card #1</td>
+                    <td>John Doe</td>
+                    <td>15:42 | 14/08/2023</td>
+                    <td>transaction</td>
+                    <td>1,000,000</td>
                     <td><span class="status declined">Declined</span></td>
                 </tr>
             </tbody>
+            
         </table>
     </div>
         <!-- End of transaction-list -->
