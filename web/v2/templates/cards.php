c6c2008088c16a5b559173cbee7e04d6779e106e
Pat101404 <pattoo10104@gmail.com>
2023-11-07 20:14:36 +0100

    mastercard logo

--- web/v2/templates/cards.php
+++ web/v2/templates/cards.php
@@ -124,6 +124,12 @@
             <div class="card">
                 <div class="header">
                     <h3 class="title">Card #2</h3>
+                    <svg xmlns="http://www.w3.org/2000/svg" class="logo" width="90" height="43" viewBox="0 0 143 88.78">
+                        <title>Mastercard logo</title>
+                        <polygon points="52.16 79.29 90.83 79.29 90.83 9.49 52.16 9.49 52.16 79.29" style="fill:#ff5f00"/>
+                        <path d="M495.12,512A44.38,44.38,0,0,1,512,477.1a44.39,44.39,0,1,0,0,69.8A44.39,44.39,0,0,1,495.12,512" transform="translate(-440.5 -467.61)" style="fill:#eb001b"/>
+                        <path d="M583.5,512A44.15,44.15,0,0,1,512,546.9a44.52,44.52,0,0,0,0-69.8A44.15,44.15,0,0,1,583.5,512Z" transform="translate(-440.5 -467.61)" style="fill:#f79e1b"/>
+                    </svg>
                 </div>
                 <div class="cardcontent">
                     <div class="row">
