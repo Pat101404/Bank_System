ccf4c85e1e7009a9aff67ef5dccc349008dac812
Pat101404 <pattoo10104@gmail.com>
2023-11-24 00:32:27 +0100

    accounts page + testing folder

---
+++ web/v2/templates/account.php
@@ -0,0 +1,68 @@
+<div class="accountrow">
+    <div class="accountpanelleft">
+        <div class="accountpaneltitle">
+            <i class="bx bx-user"></i>
+            <h3>Account Details</h3>
+        </div>
+        <div class="panelcontents">
+            <table>
+                <tr>
+                    <th>Account Name:</th>
+                    <th class="data"><p>Account A1</p></th>
+                </tr>
+                <tr>
+                    <th>Account ID:</th>
+                    <th class="data"><p>6198414</p></th>
+                </tr>
+                <tr>
+                    <th>Email:</th>
+                    <th class="data"><p>JohnDoe1234@gmail.com</p></th>
+                </tr>
+                <tr>
+                    <th>phone:</th>
+                    <th class="data"><p>+420 651 675 211</p></th>
+                </tr>
+                <tr>
+                    <th>Address:</th>
+                    <th class="data"><p>Example 1234</p></th>
+                </tr>
+                <tr>
+                    <th>Date of creation:</th>
+                    <th class="data"><p>01 / 01 / 2020</p></th>
+                </tr>
+            </table>
+        </div>
+    </div>
+    <div class="accountpanelright">
+        <div class="accountpaneltitle">
+            <i class="bx bx-user"></i>
+            <h3>Account owner</h3>
+        </div>
+        <table>
+            <tr>
+                <th>Name:</th>
+                <th class="data"><p>John Doe</p></th>
+            </tr>
+            <tr>
+                <th>Client ID:</th>
+                <th class="data"><p>61951061</p></th>
+            </tr>
+            <tr>
+                <th>Email:</th>
+                <th class="data"><p>JohnDoe1234@gmail.com</p></th>
+            </tr>
+            <tr>
+                <th>phone:</th>
+                <th class="data"><p>+420 651 675 211</p></th>
+            </tr>
+            <tr>
+                <th>Address:</th>
+                <th class="data"><p>Example 1234</p></th>
+            </tr>
+            <tr>
+                <th>Date of birth:</th>
+                <th class="data"><p>01 / 01 / 2000</p></th>
+            </tr>
+        </table>
+    </div>
+</div>
---
+++ web/v2/testing/db.php
@@ -0,0 +1,20 @@
+<?php
+$servername = "localhost"; // Replace with your MySQL server name
+$username = "root"; // Replace with your MySQL username
+$password = "Pat19982004"; // Replace with your MySQL password
+$dbname = "bank_db"; // Replace with your MySQL database name
+
+// Create a connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} else {
+    echo "Connected successfully";
+    // Perform operations or queries here
+}
+
+// Close the connection
+$conn->close();
+?>
