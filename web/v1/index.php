c6e1a10ce0d1fe3b9a2fa9ba28b4ca5c0bfbdcae
Pat101404 <pattoo10104@gmail.com>
2023-12-11 15:20:15 +0100

    update

--- web/v1/index.php
+++
@@ -1,7 +0,0 @@
-<html>
-<head>
-    <meta http-equiv="refresh" content="0; url='./login/login.php'" />
-</head>
-<body>
-</body>
-</html>
--- web/v1/templates/footer.php
+++
@@ -1,4 +0,0 @@
-<footer>
-    <p>123 Bank Street | City, State ZIP Code | Email: <a href="mailto:info@flowbank.com">info@flowbank.com</a> | Phone: +420 111222333</p>
-    <p>&copy; 2023 Flow Bank. All rights reserved.</p>
-</footer>
